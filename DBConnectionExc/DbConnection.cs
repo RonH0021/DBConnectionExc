cab8ba20516b5cd31055f7abec07297fcbe54593
RH <abc@abc.com>
2018-05-24 01:49:13 +0530

    1.0

--- DBConnectionExc/DbConnection.cs
+++ DBConnectionExc/DbConnection.cs
@@ -2,35 +2,6 @@
 
 namespace DBConnectionExc
 {
-    public class DbCommand
-    {
-        public string _command { get; set; }
-        public DbConnection _dbConnection { get; set; } 
-
-        public DbCommand(string command, DbConnection dbConnection)
-        {
-            if (dbConnection == null || command == null || String.IsNullOrWhiteSpace(command))
-            {
-                throw new InvalidOperationException("Invalid db connection");
-            }
-            else
-            {
-                _command = command;
-                _dbConnection = dbConnection;
-            }
-        }
-
-        public void Execute()
-        {
-            _dbConnection.Open();
-            Console.WriteLine("The command {0} has been executed on {1}",_command,_dbConnection._connectionString);
-            _dbConnection.Close();
-        }
-
-    }
-
-
-
     public abstract class DbConnection
     {
         public string _connectionString { get; set; }
