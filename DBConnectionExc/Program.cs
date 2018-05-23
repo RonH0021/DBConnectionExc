35b45d4baca7f06d1b2a2dc0d6c8cad056d04770
RH <abc@abc.com>
2018-05-24 01:46:03 +0530

    abc

--- DBConnectionExc/Program.cs
+++ DBConnectionExc/Program.cs
@@ -10,8 +10,12 @@ namespace DBConnectionExc
         static void Main(string[] args)
         {
             var sqlConnection = new SqlConnection("ACC_TST_CON");
-            var dbCommand = new DbCommand("SELECT * FROM DATA", sqlConnection);
-            dbCommand.Execute();
+            var sqlCommand = new DbCommand("SELECT * FROM DATA", sqlConnection);
+            sqlCommand.Execute();
+
+            var oracleConnection = new OracleConnection(@"IBM\DB2");
+            var oracleCommand = new DbCommand("SELECT * FROM ITEMS", oracleConnection);
+            oracleCommand.Execute();
         }
     }
 }
